c4023e948bb333a2ee7d91764d057926ead86d5d
Nicholas <nicholaspaulsiebenaller@gmail.com>
2024-03-13 21:59:30 -0500

    Update static export to github pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withMDX = require("@next/mdx")({
 	extension: /\.mdx?$/,
 });
@@ -5,7 +7,9 @@ const withMDX = require("@next/mdx")({
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
 	reactStrictMode: true,
+	output: "export",
+	images: { unoptimized: true },
 	pageExtensions: ["ts", "tsx", "mdx"],
-	basePath: "/blog",
-	assetPrefix: "/blog",
+	basePath: isProd ? "/blog" : undefined,
+	assetPrefix: isProd ? "/blog" : undefined,
 });
